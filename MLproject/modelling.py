deafd113e85da0e59444bded337b6e5f7add8f35
Salman Fatahillah <61404437+esaelemaen@users.noreply.github.com>
2026-01-01 19:31:06 +0700

    Remove MLflow tracking URI configuration
    Removed tracking URI setup for MLflow.

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -25,9 +25,6 @@ if __name__ == "__main__":
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
 
-    tracking_uri = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mlruns")
-    mlflow.set_tracking_uri(tracking_uri)
-
  
     with mlflow.start_run(nested=True) as run:
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
